6ce2b9058685378820c9d62089751f0887214363
milky <listingslab@gmail.com>
2022-06-09 00:05:43 +0200

    Flash

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
 let uniqueArr = []
-
-function removeArrayDuplicates(arr){
-    // Accepts an array from which the duplicates
-    // will be removed
+function uniqifyArr(arr){
     if (!Array.isArray(arr)){  
         arr = []
     }
-
     let theSet = new Set(arr)
     let uniqueArr = [...theSet]
     return uniqueArr
 }
 
-// Testing Only
-// let myNums = [1,2,3,1,4,1,2,5,3,4]
-// let uniqueNums = removeArrayDuplicates(myNums)
-// console.log(uniqueNums)
+// Tests 
+let myNums = [1,2,3,1,4,1,2,5,3,4]
+let uniqueNums = uniqifyArr(myNums)
+console.warn(uniqueNums)
 
-module.exports = removeArrayDuplicates
+module.exports = uniqifyArr
